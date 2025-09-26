1bbe01003241514515c93c1ed83cb8d1cc4d9cef
Michael Åberg <ZuraK@users.noreply.github.com>
2025-09-26 07:35:36 +0200

    Update inherits.c

--- lpc/inherits.c
+++ lpc/inherits.c
@@ -1,6 +1,7 @@
 // 2025-09-26 Created
 
 // TODO
+// brain not working
 // * Use ? for list of local valid id's
 // * INHLIST_FLAT | INHLIST_TREE | INHLIST_TAG_VIRTUAL
 
@@ -23,11 +24,42 @@ void print_indent( )
 {
 }
 
+/*
+  case:direct_inherit ({"0.0",({"1.0","1.1","1.2","1.3"}) })
+  [0.0]
+  ├──[1.0]
+  ├──[1.1]
+  ├──[1.2]
+  └──[1.3]
+*/
+
 // Fixme
-void print_tree( mixed *data, int level )
+void print_tree( mixed *data, int depth )
 {
+   // Data present?
   if( !data || sizeof( data ) == 0 )
     return;
+
+   // Total number of items at current depth
+   int total_items = sizeof( data );
+
+   // Fixme
+   for( int i = 0; i < total_items; i++ )
+   {
+      if( stringp( data[i] ) )
+      {
+         // Is item
+      }
+      else if( pointerp( data[i] ) )
+      {
+         // Is array, recursion
+         print_tree( data[i], depth + 1 );
+      }
+      else
+      {
+         // Should not be here
+      }
+   }
 }
 
 execute( str )
@@ -39,7 +71,7 @@ execute( str )
     return 1;
   }
 
-  // Check if target is present?
+  // Check if object is present?
   object *obs;
   object ob;
   obs = all_inventory( environment( this_player() ) );
@@ -49,13 +81,14 @@ execute( str )
       ob = obs[i];
   }
 
+  // Object not found.
   if( !ob )
   {
     write( "Object \"" + str + "\" not found.\n" );
     return 1;
   }
 
-  // Get inherits
+  // Get inherits of object
   mixed *data = inherit_list( ob, INHLIST_TREE | INHLIST_TAG_VIRTUAL );
 
   print_tree( data, 0 );
